e8145ff4673b97f7a4630277b6ed2550fd5f9f1a
ZamiRich <argashokovzamir@gmail.com>
2020-12-25 18:42:54 +0100

    Initial Commit

---
+++ FifthLesson/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  FifthLesson
+//
+//  Created by ZamiRich on 25.12.2020.
+//  Copyright © 2020 ZamiRich. All rights reserved.
+//
+
+import Foundation
+
+print("Hello, World!")
+
